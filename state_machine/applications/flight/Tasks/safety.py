75612dad450f144fab9eb9868e3b06ae87c0fa71
thetazero <lesha.seletskiy@gmail.com>
2022-12-04 17:34:52 -0500

    Lint

--- state_machine/applications/flight/Tasks/safety.py
+++ state_machine/applications/flight/Tasks/safety.py
@@ -20,7 +20,8 @@ class task(Task):
             self.debug(f'Temp too high ({temp:.1f}°C >= {cubesat.HIGH_TEMP - 1:.1f}°C)', log=True)
         else:
             self.debug_status(vbatt, temp)
-            self.debug(f'Safe operating conditions reached, switching back to {state_machine.previous_state} mode', log=True)
+            self.debug(
+                f'Safe operating conditions reached, switching back to {state_machine.previous_state} mode', log=True)
             state_machine.switch_to(state_machine.previous_state)
 
     def other_modes(self, vbatt, temp):
