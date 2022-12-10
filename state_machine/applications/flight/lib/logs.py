a07996118a187ff2fe429da1f52b5d1f8a3c0df0
Jacob Willis <jbwillis272@gmail.com>
2022-12-10 15:20:05 -0500

    beacon data is more useful

--- state_machine/applications/flight/lib/logs.py
+++ state_machine/applications/flight/lib/logs.py
@@ -1,5 +1,4 @@
 from pycubed import cubesat
-from state_machine import state_machine
 import struct
 try:
     from ulab.numpy import array
@@ -7,42 +6,42 @@ except ImportError:
     from numpy import array
 
 
-beacon_format = 'b' + 'f' * 11  # 1 char + 11 floats
+beacon_format = 'H' + 'f' * 11  # 1 short + 11 floats
 
-def beacon_packet(task):
-    """Creates a beacon packet containing the: CPU temp, IMU temp, gyro, acceleration, magnetic, and state byte.
-    The state byte is the index of the current state in the alphabetically ordered state list.
+def beacon_packet():
+    """Creates a beacon packet containing the: boot count, battery voltage, 
+    CPU temperature, IMU temperature, gyro reading, mag reading, 
+    radio signal strength (RSSI), radio frequency error (FEI).
+
     This data is packed into a c struct using `struct.pack`.
-
-    If no IMU is attached it returns a packet of 0s.
     """
-    if not cubesat.imu:
-        task.debug('IMU not initialized')
-        return bytes([0, 0, 0, 0, 0])
 
+    boot_count = cubesat.c_boot
+    vbatt = cubesat.battery_voltage
     cpu_temp = cubesat.temperature_cpu
     imu_temp = cubesat.temperature_imu
     gyro = cubesat.gyro
-    acc = cubesat.acceleration
     mag = cubesat.magnetic
-    state_byte = state_machine.states.index(state_machine.state)
+    rssi = cubesat.radio.last_rssi
+    fei = cubesat.radio.frequency_error
     return struct.pack(beacon_format,
-                       state_byte, cpu_temp, imu_temp,
+                       boot_count, vbatt,
+                       cpu_temp, imu_temp,
                        gyro[0], gyro[1], gyro[2],
-                       acc[0], acc[1], acc[2],
-                       mag[0], mag[1], mag[2])
+                       mag[0], mag[1], mag[2],
+                       rssi, fei)
 
 def unpack_beacon(bytes):
     """Unpacks the fields from the beacon packet packed by `beacon_packet`
     """
 
-    (state_byte, cpu_temp, imu_temp,
+    (boot_count, vbatt,
+     cpu_temp, imu_temp,
      gyro0, gyro1, gyro2,
-     acc0, acc1, acc2,
-     mag0, mag1, mag2) = struct.unpack(beacon_format, bytes)
+     mag0, mag1, mag2,
+     rssi, fei) = struct.unpack(beacon_format, bytes)
 
     gyro = array([gyro0, gyro1, gyro2])
-    acc = array([acc0, acc1, acc2])
     mag = array([mag0, mag1, mag2])
 
-    return state_byte, cpu_temp, imu_temp, gyro, acc, mag
+    return boot_count, vbatt, cpu_temp, imu_temp, gyro, mag, rssi, fei
--- state_machine/unit_tests/test_logs.py
+++ state_machine/unit_tests/test_logs.py
@@ -10,38 +10,44 @@ sys.path.insert(0, './state_machine/frame')
 from lib.logs import beacon_packet, unpack_beacon
 from pycubed import cubesat
 from state_machine import state_machine
-from lib.template_task import Task
 
 class TestLogs(unittest.TestCase):
 
     def test(self):
 
-        task = Task()
-
-        accel_in = array([1.0, 2.0, 3.0])
         mag_in = array([4.0, 3.0, 1.0])
         gyro_in = array([-42.0, 0.1, 7.0])
 
         cpu_temp_in = 77
         imu_temp_in = 22
 
-        cubesat._accel = accel_in
+        boot_count_in = 453
+        vbatt_in = 3.45
+
+        rssi_in = -88.8
+        fei_in = -987.0
+
+        cubesat.c_boot = boot_count_in
+        cubesat.LOW_VOLTAGE = vbatt_in
+        cubesat.randomize_voltage = False
+
         cubesat._mag = mag_in
         cubesat._gyro = gyro_in
         cubesat._cpu_temp = cpu_temp_in
         cubesat._imu_temperature = imu_temp_in
 
-        state_machine.states = [1, 2, 3, 4]
-        state_machine.state = 2
-        state_in = state_machine.states.index(state_machine.state)
+        cubesat.radio._last_rssi = rssi_in
+        cubesat.radio._frequency_error = fei_in
 
-        pkt = beacon_packet(task)
+        pkt = beacon_packet()
 
-        state_out, cpu_temp_out, imu_temp_out, gyro_out, accel_out, mag_out = unpack_beacon(pkt)
+        boot_count_out, vbatt_out, cpu_temp_out, imu_temp_out, gyro_out, mag_out, rssi_out, fei_out = unpack_beacon(pkt)
 
-        self.assertEqual(state_in, state_out)
-        self.assertEqual(cpu_temp_in, cpu_temp_out)
-        self.assertEqual(imu_temp_in, imu_temp_out)
-        testing.assert_array_almost_equal(accel_in, accel_out)
+        self.assertEqual(boot_count_in, boot_count_out)
+        self.assertAlmostEqual(vbatt_in + 0.01, vbatt_out, places=5)
+        self.assertAlmostEqual(cpu_temp_in, cpu_temp_out, places=5)
+        self.assertAlmostEqual(imu_temp_in, imu_temp_out, places=5)
+        self.assertAlmostEqual(rssi_in, rssi_out, places=5)
+        self.assertAlmostEqual(fei_in, fei_out, places=5)
         testing.assert_array_almost_equal(gyro_in, gyro_out)
         testing.assert_array_almost_equal(mag_in, mag_out)
