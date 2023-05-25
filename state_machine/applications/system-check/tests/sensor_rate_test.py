e00c9754bc8117b84d74a1b482dea2fc51b42b87
tomyyyD <thomas5d@comcast.net>
2023-05-25 08:12:59 -0400

    convert to seconds instead of ns

--- state_machine/applications/system-check/tests/sensor_rate_test.py
+++ state_machine/applications/system-check/tests/sensor_rate_test.py
@@ -6,22 +6,22 @@ except ImportError:
 from lib.pycubed import cubesat
 
 def sensor_rate_test(sensor, name):
-    trials = 1000
+    trials = 100
     li = [0.0] * trials
     dt_list = array(li)
     if name == "IMU":
         for i in range(trials):
-            s_time = time.monotonic_ns()
+            s_time = time.monotonic()
             _ = sensor.accel
             _ = sensor.accel
-            e_time = time.monotonic_ns()
+            e_time = time.monotonic()
             dt_list[i] = e_time - s_time
     else:
         for i in range(trials):
-            s_time = time.monotonic_ns()
+            s_time = time.monotonic()
             _ = sensor.lux
             _ = sensor.lux
-            e_time = time.monotonic_ns()
+            e_time = time.monotonic()
             dt_list[i] = e_time - s_time
     worst_case = max(dt_list)
     best_case = min(dt_list)
@@ -39,7 +39,7 @@ async def run(result_dict):
         (cubesat.sun_xp, "SUN_X+"),
         (cubesat.sun_yn, "SUN_Y-"),
         (cubesat.sun_yp, "SUN_Y+"),
-        # (cubesat.sun_zn, "SUN_Z-"),
+        (cubesat.sun_zn, "SUN_Z-"),
         (cubesat.sun_zp, "SUN_Z+"),
         (cubesat.imu, "IMU"),
     ]
