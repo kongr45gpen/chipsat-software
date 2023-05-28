42cda18ae75a124ef374ac4afee3aaf033233d5f
tomyyyD <thomas5d@comcast.net>
2023-05-28 16:46:18 -0400

    Using lambda's to generlize test function
    each lensor is just represented as the lambda function which is the property I want to test

--- state_machine/applications/system-check/tests/sensor_rate_test.py
+++ state_machine/applications/system-check/tests/sensor_rate_test.py
@@ -9,20 +9,12 @@ def sensor_rate_test(sensor, name):
     trials = 100
     li = [0.0] * trials
     dt_list = array(li)
-    if name == "IMU":
-        for i in range(trials):
-            s_time = time.monotonic()
-            _ = sensor.accel
-            _ = sensor.accel
-            e_time = time.monotonic()
-            dt_list[i] = e_time - s_time
-    else:
-        for i in range(trials):
-            s_time = time.monotonic()
-            _ = sensor.lux
-            _ = sensor.lux
-            e_time = time.monotonic()
-            dt_list[i] = e_time - s_time
+    for i in range(trials):
+        s_time = time.monotonic()
+        _ = sensor
+        _ = sensor
+        e_time = time.monotonic()
+        dt_list[i] = e_time - s_time
     worst_case = max(dt_list)
     best_case = min(dt_list)
     med_case = median(dt_list)
@@ -35,13 +27,15 @@ async def run(result_dict):
     """
     print("Testing sensor rates...\n")
     sensors = [
-        (cubesat.sun_xn, "SUN_X-"),
-        (cubesat.sun_xp, "SUN_X+"),
-        (cubesat.sun_yn, "SUN_Y-"),
-        (cubesat.sun_yp, "SUN_Y+"),
-        (cubesat.sun_zn, "SUN_Z-"),
-        (cubesat.sun_zp, "SUN_Z+"),
-        (cubesat.imu, "IMU"),
+        (cubesat.sun_xn.lux, "SUN_X-"),
+        (cubesat.sun_xp.lux, "SUN_X+"),
+        (cubesat.sun_yn.lux, "SUN_Y-"),
+        (cubesat.sun_yp.lux, "SUN_Y+"),
+        (cubesat.sun_zn.lux, "SUN_Z-"),
+        (cubesat.sun_zp.lux, "SUN_Z+"),
+        (cubesat.imu.accel, "IMU_ACCEL"),
+        (cubesat.imu.gyro, "IMU_GYRO"),
+        (cubesat.imu.mag, "IMU_MAG"),
     ]
     for (sensor, name) in sensors:
         result_dict[f"{name}_rate_test"] = sensor_rate_test(sensor, name)
