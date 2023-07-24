603f473ce5dff24ccb79dda6b301dc2c2801d83b
tomyyyD <thomas5d@comcast.net>
2023-07-24 13:33:49 -0400

    making the emulation camera driver better

--- drivers/emulation/lib/camera_driver.py
+++ drivers/emulation/lib/camera_driver.py
@@ -1,10 +1,26 @@
+from lib.image_utils import flags
+import time
+import random
 
 class Camera:
     def __init__(self) -> None:
         self.buffer = bytearray(10)
+        self.firstPacket = True
+        self.packets_sent = 0
 
+    @property
     def get_packet(self):
-        return self.buffer
+        time.sleep(0.1)
+        rand_num = random.random()
+        if rand_num < 0.05:
+            return None, flags.FAIL_NO_PACKET
+        self.packets_sent += 1
+        if self.firstPacket:
+            return self.buffer, flags.SUCCESS_FIRST_PACKET
+        if self.packets_sent > 50:
+            return self.buffer, flags.SUCCESS_END_PACKET
+        return self.buffer, flags.SUCCESS_MID_PACKET
 
     def get_confirmation(self):
+        time.sleep(2)
         return True
